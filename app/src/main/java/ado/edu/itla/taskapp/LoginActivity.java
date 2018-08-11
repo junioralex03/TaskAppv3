5347e77df7ba9db21c8ed8e5e07d6a9b81078800
Ronald <lareplica931472@gmail.com>
2018-08-11 11:03:21 -0400

    FInal API

--- app/src/main/java/ado/edu/itla/taskapp/LoginActivity.java
+++ app/src/main/java/ado/edu/itla/taskapp/LoginActivity.java
@@ -1,13 +1,22 @@
 package ado.edu.itla.taskapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.content.Intent;
 import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
 import ado.edu.itla.taskapp.entidad.Usuario;
 import ado.edu.itla.taskapp.entidad.UsuarioActivo;
@@ -18,7 +27,7 @@ import ado.edu.itla.taskapp.vista.TecnicoActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = "LogInActivity";
+    private static final String LOG_TAG = "LoginActivity";
     private UsuarioRepositorio usuarioRepositorio;
     private Usuario usuario;
 
@@ -37,8 +46,52 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        {
+            final TextView mTextView = (TextView) findViewById(R.id.text);
+// ...
+
+// Instantiate the RequestQueue.
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url = "http://192.168.200.150:8080";
+            final Gson gson = new Gson();
+// Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+
+                            Log.i("RESPONSE", response);
+                            Usuario g = gson.fromJson(response, Usuario.class);
+
+                            // Display the first 500 characters of the response string.
+                            mTextView.setText("Response is: " + response.substring(0, 500));
+                        }
+                    }, new Response.ErrorListener() {
 
 
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.i("RESPONSE", error.toString());
+
+                    mTextView.setText("That didn't work!");
+                }
+            }) {
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+
+                    Gson gson = new Gson();
+                    String json = gson.toJson(usuario);
+
+                    return json.getBytes();
+                }
+            };
+
+
+// Add the request to the RequestQueue.
+            queue.add(stringRequest);
+            ;
+        }
 
 
         Button btnEntrar = findViewById(R.id.btnEntrar);
